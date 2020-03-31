fd83f00461c36ace0f3276f887c55d6c61715f2d
srdpatel <sagarpatel288@gmail.com>
2020-03-31 23:49:41 +0530

    19.0 Add demand and info icon to item list 19.1 Initiate menus in navigation drawer for orders and reports 19.2 Change header color to green gradient in navigation drawer 19.3 Add relevant string resources

--- app/src/main/java/com/browngold/khedubhai/user/NavigationDrawerActivity.kt
+++ app/src/main/java/com/browngold/khedubhai/user/NavigationDrawerActivity.kt
@@ -36,7 +36,7 @@ class NavigationDrawerActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_orders, R.id.nav_reports
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
