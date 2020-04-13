355c3d9ccf3a49c9b1e1e628e612889380b8390a
srdpatel <sagarpatel288@gmail.com>
2020-04-13 06:22:22 +0530

    41.0 Binding logIn xml with viewModel considering baseFormViewModel and baseState

--- app/src/main/java/com/browngold/khedubhai/user/ui/loginsignup/login/LogInViewModel.kt
+++ app/src/main/java/com/browngold/khedubhai/user/ui/loginsignup/login/LogInViewModel.kt
@@ -3,11 +3,11 @@ package com.browngold.khedubhai.user.ui.loginsignup.login
 import androidx.lifecycle.MutableLiveData
 import com.browngold.khedubhai.user.api.ApiService
 import com.browngold.khedubhai.user.api.Apis
-import com.example.android.common.baseapp.BaseViewModel
+import com.example.android.common.baseviewmodels.BaseFormViewModel
 import org.koin.core.inject
 import org.koin.core.parameter.parametersOf
 
-class LogInViewModel : BaseViewModel() {
+class LogInViewModel : BaseFormViewModel() {
 
     val emailMutableLiveData = MutableLiveData<String>("")
     val countryCodeMutableLiveData = MutableLiveData<String>("")
@@ -15,6 +15,24 @@ class LogInViewModel : BaseViewModel() {
     val uniqueIdMutableLiveData = MutableLiveData<String>("")
     val passwordMutableLiveData = MutableLiveData<String>("")
 
-    private val apiService: ApiService by inject { parametersOf(Apis.BASE_URL, ApiService::class.java) }
+    private val apiService: ApiService by inject {
+        parametersOf(
+            Apis.BASE_URL,
+            ApiService::class.java
+        )
+    }
+
+    fun onClickForgotPassword(
+        email: String,
+        countryCode: String,
+        mobileNumber: String,
+        uniqueId: String
+    ) {
+        // comment by srdpatel: 4/13/2020 Check: email, country code, mobile number and unique id
+    }
+
+    fun onClickForgotUniqueId() {
+
+    }
 
 }
