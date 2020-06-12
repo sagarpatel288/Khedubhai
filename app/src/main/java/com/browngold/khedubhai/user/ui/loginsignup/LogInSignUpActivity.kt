8a97be85f2ef9bc27dfe885a409d6ea35e4bf233
srdpatel <sagarpatel288@gmail.com>
2020-06-12 09:54:32 +0530

    59.0 Upgrade & Refactor: .gitignore, gradle and LogInSignUpActivity

--- app/src/main/java/com/browngold/khedubhai/user/ui/loginsignup/LogInSignUpActivity.kt
+++ app/src/main/java/com/browngold/khedubhai/user/ui/loginsignup/LogInSignUpActivity.kt
@@ -1,8 +1,6 @@
 package com.browngold.khedubhai.user.ui.loginsignup
 
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import androidx.databinding.ViewDataBinding
 import com.browngold.khedubhai.user.R
 import com.browngold.khedubhai.user.databinding.ActivityLogInSignUpBinding
@@ -10,7 +8,8 @@ import com.example.android.common.baseapp.BaseActivity
 import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
 
-class LogInSignUpActivity : BaseActivity<ActivityLogInSignUpBinding, LogInSignUpViewModel>(R.layout.activity_log_in_sign_up) {
+class LogInSignUpActivity :
+    BaseActivity<ActivityLogInSignUpBinding, LogInSignUpViewModel>(R.layout.activity_log_in_sign_up) {
 
     override val viewModel: LogInSignUpViewModel by viewModel()
     private val sharedPrefs: SharedPreferences by inject()
@@ -20,6 +19,10 @@ class LogInSignUpActivity : BaseActivity<ActivityLogInSignUpBinding, LogInSignUp
     }
 
     override fun otherStuffs() {
+
+    }
+
+    override fun setObservers() {
 
     }
 }
