4dbe1be72164b47187257a371840e5ff211ef155
srdpatel <sagarpatel288@gmail.com>
2020-06-13 11:30:20 +0530

    62.0 Refactor: subsequent effect of submodule common commit id: 75.0, 76.0 & 77.0 commit id: 62.0

--- app/src/main/java/com/browngold/khedubhai/user/ui/loginsignup/LogInSignUpViewModel.kt
+++ app/src/main/java/com/browngold/khedubhai/user/ui/loginsignup/LogInSignUpViewModel.kt
@@ -1,6 +1,6 @@
 package com.browngold.khedubhai.user.ui.loginsignup
 
-import com.example.android.common.baseapp.BaseViewModel
+import com.example.android.common.baseviewmodels.BaseViewModel
 
 class LogInSignUpViewModel : BaseViewModel() {
 }
